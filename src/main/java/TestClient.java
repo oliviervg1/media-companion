b7f8f16dd11a8f9890deb2e7744848f83fe41fde
Olivier Van Goethem <ov109@ic.ac.uk>
2014-10-04 15:33:21 +0100

    Linting code

--- src/main/java/TestClient.java
+++ src/main/java/TestClient.java
@@ -5,33 +5,33 @@ import javax.xml.ws.Service;
 import automation.api.interfaces.ConnectedDevice;
 
 public class TestClient {
- 
-	private static final String WS_URL = "http://192.168.0.234:8080/media-companion-1.0.0/MediaPlayer?wsdl";
- 
-	public static void main(String[] args) throws Exception {
- 
-		URL url = new URL(WS_URL);
+
+    private static final String WS_URL = "http://192.168.0.234:8080/media-companion-1.0.0/MediaPlayer?wsdl";
+
+    public static void main(String[] args) throws Exception {
+
+        URL url = new URL(WS_URL);
         QName qname = new QName("http://media.pi.com/", "MediaPlayerService");
- 
+
         Service service = Service.create(url, qname);
         ConnectedDevice mediaPlayer = service.getPort(ConnectedDevice.class);
- 
+
         while(true) {
-        	System.out.println("Play/Pause/Close?");
-        	String event = System.console().readLine();
-        	
-        	if (event.equalsIgnoreCase("play")) {
-        		String fileToPlay = "http://www.stuart-holland.com:8080/uploads/Hunger Game.mp4";
-        		fileToPlay = fileToPlay.replace(" ", "%20");
-        		Object parameters[] = {fileToPlay};
-        		mediaPlayer.invokeMethod("playTrack", parameters);
-        	}
-        	if (event.equalsIgnoreCase("pause")) {
-        		mediaPlayer.invokeMethod("togglePlay");
-        	}
-        	if (event.equalsIgnoreCase("close")) {
-        		mediaPlayer.invokeMethod("close");
-        	}
-        } 
+            System.out.println("Play/Pause/Close?");
+            String event = System.console().readLine();
+
+            if (event.equalsIgnoreCase("play")) {
+                String fileToPlay = "http://www.stuart-holland.com:8080/uploads/Hunger Game.mp4";
+                fileToPlay = fileToPlay.replace(" ", "%20");
+                Object parameters[] = {fileToPlay};
+                mediaPlayer.invokeMethod("playTrack", parameters);
+            }
+            if (event.equalsIgnoreCase("pause")) {
+                mediaPlayer.invokeMethod("togglePlay");
+            }
+            if (event.equalsIgnoreCase("close")) {
+                mediaPlayer.invokeMethod("close");
+            }
+        }
     }
 }
--- src/main/java/com/pi/media/MediaPlayer.java
+++ src/main/java/com/pi/media/MediaPlayer.java
@@ -9,57 +9,57 @@ import automation.api.components.JMPlayer;
 
 @WebService(endpointInterface = "automation.api.interfaces.ConnectedDevice")
 public class MediaPlayer extends AbstractDevice {
-	
-	private JMPlayer player;
-	private float volume;
-	
-	@Override
-	protected void onStartup() {
-		player = new JMPlayer("/Users/olivier/Downloads/MPlayer-1.1/", "-slave -idle", "/var/tmp/youtube-dl-cookies.txt");
-		volume = 80;
-	}
-	
-	public void playTrack(String fileToPlay) {
-		if (player.isPlaying()) {
-			player.close();
-		}
-		try {
-			player.open(fileToPlay);
-			player.setVolume(volume);
-			player.setFullScreen(true);
-		} catch (IOException e) {}
-	}
-	
-	public void togglePlay() {
-		player.togglePlay();
-	}
-	
-	public void setVolume(float volume) {
-		this.volume = volume;
-		player.setVolume(volume);
-	}
-	
-	public float getVolume() {
-		return player.getVolume();
-	}
-	
-	public void setTimePosition(long seconds) {
-		player.setTimePosition(seconds);
-	}
-	
-	public long getTimePosition() {
-		return player.getTimePosition();
-	}
-	
-	public long getTotalTime() {
-		return player.getTotalTime();
-	}
-	
-	public void close() {
-		player.close();
-	}
-	
-	public String getPlayingFile() {
-		return player.getPlayingFile();
-	}
+
+    private JMPlayer player;
+    private float volume;
+
+    @Override
+    protected void onStartup() {
+        player = new JMPlayer("/Users/olivier/Downloads/MPlayer-1.1/", "-slave -idle", "/var/tmp/youtube-dl-cookies.txt");
+        volume = 80;
+    }
+
+    public void playTrack(String fileToPlay) {
+        if (player.isPlaying()) {
+            player.close();
+        }
+        try {
+            player.open(fileToPlay);
+            player.setVolume(volume);
+            player.setFullScreen(true);
+        } catch (IOException e) {}
+    }
+
+    public void togglePlay() {
+        player.togglePlay();
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        player.setVolume(volume);
+    }
+
+    public float getVolume() {
+        return player.getVolume();
+    }
+
+    public void setTimePosition(long seconds) {
+        player.setTimePosition(seconds);
+    }
+
+    public long getTimePosition() {
+        return player.getTimePosition();
+    }
+
+    public long getTotalTime() {
+        return player.getTotalTime();
+    }
+
+    public void close() {
+        player.close();
+    }
+
+    public String getPlayingFile() {
+        return player.getPlayingFile();
+    }
 }
